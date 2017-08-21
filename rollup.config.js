ac1bab1ddf1266828564fff4f9754eb8ff6d8005
Robin van der Vleuten <robin@webstronauts.co>
2017-08-21 20:07:52 +0200

    Tweaked rollup config somewhat more

--- rollup.config.js
+++ rollup.config.js
@@ -2,27 +2,22 @@ const babel = require('rollup-plugin-babel');
 const rollup = require('rollup');
 
 const pkg = require('./package.json');
-const format = process.env.FORMAT;
-
-// prettier-ignore
-const banner =
-  '/*!\n' +
-  ' * hippalus.js v' + pkg.version + '\n' +
-  ' * (c) ' + new Date().getFullYear() + ' Robin van der Vleuten <robin@webstronauts.co>\n' +
-  ' * Released under the MIT License.\n' +
-  ' */';
 
 module.exports = {
   entry: 'src/index.js',
   moduleName: 'hippalus',
   dest: pkg.main,
-  banner,
   useStrict: false,
   format: 'umd',
+  exports: 'named',
   external: ['react', 'path-to-regexp'],
+  globals: {
+    'path-to-regexp': 'Regexp',
+    react: 'React',
+  },
   plugins: [
     babel({
-      presets: [['env', { modules: false }], 'flow'],
+      presets: [['env', { modules: false }]],
       exclude: 'node_modules/**',
       babelrc: false,
     }),
